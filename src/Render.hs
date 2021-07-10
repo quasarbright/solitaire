dbc0847039eb38778b02c6c9f01d976f36393ff3
Mike Delmonaco <mthundershock@gmail.com>
2021-07-10 18:05:00 -0400

    start rendering

---
+++ src/Render.hs
@@ -0,0 +1,63 @@
+module Render where
+
+import Game
+import Card
+import Text.PrettyPrint.Boxes
+import Data.Foldable
+import Control.Lens
+
+{-
+Deck: ??
+Draw: VS
+Foundations: VS VS VS VS
+Piles:
+VS ?? ??
+   VS ??
+      VS
+
+1  2  3
+
+
+hidden cards are ??
+blank spots are --
+
+-}
+
+bCard :: Card -> Box
+bCard = text . show
+
+bCardsV :: [Card] -> Box
+bCardsV = vcat right . fmap bCard
+
+bPile :: Pile -> Box
+bPile (Pile [] []) = text "--"
+bPile (Pile hiddens showns) = vcat right (replicate (length hiddens) (text "??")) // bCardsV (reverse showns)
+
+bPiles :: [Pile] -> Box
+bPiles piles = hsep 1 top (fmap bPile piles)
+
+bStack :: [Card] -> Box
+bStack [] = text "--"
+bStack (c:_) = bCard c
+
+bStackHidden :: [Card] -> Box
+bStackHidden [] = text "--"
+bStackHidden _ = text "??"
+
+bDeck :: Deck -> Box
+bDeck = bStackHidden
+
+bDraws :: Draws -> Box
+bDraws = bStack
+
+bFoundation :: Foundation -> Box
+bFoundation = bStack
+
+bFoundations :: (Foundation, Foundation, Foundation, Foundation) -> Box
+bFoundations (a,b,c,d) = hsep 1 top (fmap bFoundation [a,b,c,d])
+
+bGame :: Game -> Box
+bGame (Game deck draws foundations piles) = undefined
+
+renderGame :: Game -> String
+renderGame = render . bGame
